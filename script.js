734a99a7be244e8a68dc3564aa30c53926c377f3
LambertoNeri <neri.lamberto91@gmail.com>
2023-05-16 18:24:24 +0200

    add bonus

--- script.js
+++ script.js
@@ -3,18 +3,22 @@ const app = Vue.createApp ({
     data() {
         return{
             randomEmail: [
-                
+
             ],
 
         }
     },
     methods: {
         getRandomEmail(){
-            for (i = 0; i <= 10; i++ ) {
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then(serverResponse => this.randomEmail.push(serverResponse.data.response));
-            console.log (this.randomEmail);
+            this.randomEmail = [];
+            for (i = 0; i <= 9; i++ ) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(serverResponse => this.randomEmail.push(
+                    serverResponse.data.response
+                    
+                ));
             };
+            console.log(this.randomEmail)
         },
     },
     created() {
