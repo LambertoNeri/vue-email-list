3847a4943a14cb604232088b62da07eaa7e212ae
LambertoNeri <neri.lamberto91@gmail.com>
2023-05-16 17:52:26 +0200

    Exercise start

---
+++ script.js
@@ -0,0 +1,26 @@
+
+const app = Vue.createApp ({
+    data() {
+        return{
+            randomEmail: {
+
+            },
+
+        }
+    },
+    methods: {
+        getRandomEmail(){
+            for (i = 0; i <= 10; i++ ) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(serverResponse => this.randomEmail.push = [serverResponse.data.response]);
+            console.log (this.randomEmail);
+            };
+        },
+    },
+    created() {
+        this.getRandomEmail();
+    }
+});
+
+app.mount('#app');
+
