3fb358725098d3ab1d56116271bc8bca77161db4
LambertoNeri <neri.lamberto91@gmail.com>
2023-05-16 18:02:45 +0200

    last fix

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 const app = Vue.createApp ({
     data() {
         return{
-            randomEmail: {
+            randomEmail: [
 
-            },
+            ]
 
         }
     },
@@ -12,7 +12,7 @@ const app = Vue.createApp ({
         getRandomEmail(){
             for (i = 0; i <= 10; i++ ) {
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then(serverResponse => this.randomEmail.push = serverResponse.data.response);
+            .then(serverResponse => this.randomEmail.push() = serverResponse.data.response);
             console.log (this.randomEmail);
             };
         },
